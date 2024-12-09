4b7a91b0d9813b8dbabdd4048de855abd1e6eddb
Szymon Bartkowiak <szymon.b4310@gmail.com>
2024-12-09 13:10:10 +0100

    init

---
+++ src/main/java/com/example/jeniknssp/JeniknsSpApplication.java
@@ -0,0 +1,13 @@
+package com.example.jeniknssp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JeniknsSpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JeniknsSpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jeniknssp/JeniknsSpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jeniknssp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JeniknsSpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
